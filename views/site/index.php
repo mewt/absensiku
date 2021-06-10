2ea908b55f4bffae6d059d2f605a134617bbb8a9
mewt <agungsukariman@gmail.com>
2021-06-11 02:18:56 +0700

    update index.php content fin

--- views/site/index.php
+++ views/site/index.php
@@ -9,14 +9,14 @@ $this->title = 'Home';
         <h1>Hi , Admin</h1>
 
         <p class="lead">Selamat Datang di Aplikasi Absensi Pegawai.</p>
-        <p>
+
+    </div>
+    <p>
                   Sistem aplikasi absen pegawai adalah sebuah aplikasi yang dapat membantu manajemen dalam melakukan peniliaian performa pegawai berdasarkan presentase kehadiran dan memudahkan pegawai dalam melakukan kegiatan pencatatan kehadiran dan absensi di luar kantor.
                  </p>
                 <p>
                 Sistem aplikasi absen pegawai ini sebagai jawaban terhadap Manajemen Kepegawaian untuk memantapkan administrasi Kepegawaian sebagai upaya untuk memenuhi kebutuhan informasi data pegawai yang cepat, tepat, akuntabel, dan up to date. 
                 </p>
-
-    </div>
     <div class="body-content">
     <div class="row">
             <div class="col-lg-4">
@@ -37,10 +37,10 @@ $this->title = 'Home';
                 <p><a class="btn btn-default" href="/attendance">Cek Data Absens</a></p>
             </div>
    
-    </div>
+      </div>
     <div class="row">
             <div class="col-lg-4">
-                <h2>Data Absen</h2>
+                <h2>Log Out</h2>
 
                 <p> Admin dapat keluar dari aplikasi dengan menggunakan tombol logout
                 </p>
@@ -48,8 +48,6 @@ $this->title = 'Home';
             </div>
    
     </div>
-
-
-
+</div>
 
 </div>
