814a5d628fb53981084fc9abc4bb981a217c0740
mewt <agungsukariman@gmail.com>
2021-06-11 01:52:27 +0700

    update index.php content

--- views/site/index.php
+++ views/site/index.php
@@ -10,34 +10,31 @@ $this->title = 'Home';
         <h2>Hi ,  Admin Selamat Datang di Aplikasi Absensi Pegawai</h2>
     </div>
     <div class="body-content">
-    <p>
-    Sistem aplikasi absen pegawai adalah sebuah aplikasi yang dapat membantu manajemen dalam melakukan peniliaian performa pegawai berdasarkan presentase kehadiran dan memudahkan pegawai dalam melakukan kegiatan pencatatan kehadiran dan absensi di luar kantor.
-    </p>
-    <p>
-    Sistem aplikasi absen pegawai ini sebagai jawaban terhadap Manajemen Kepegawaian untuk memantapkan administrasi Kepegawaian sebagai upaya untuk memenuhi kebutuhan informasi data pegawai yang cepat, tepat, akuntabel, dan up to date. 
-    </p>
-
-    <p>
-    Hal dibawah ini adalah yang bisa lakukan oleh Admin Sistem Aplikasi Absen Pegawai 
-    </p>
+        <p>Sistem aplikasi absen pegawai adalah sebuah aplikasi yang dapat membantu manajemen dalam melakukan peniliaian performa pegawai berdasarkan presentase kehadiran dan memudahkan pegawai dalam melakukan kegiatan pencatatan kehadiran dan absensi di luar kantor.</p>
+        <p> Sistem aplikasi absen pegawai ini sebagai jawaban terhadap Manajemen Kepegawaian untuk memantapkan administrasi Kepegawaian sebagai upaya untuk memenuhi kebutuhan informasi data pegawai yang cepat, tepat, akuntabel, dan up to date. </p>
+         <p>Hal dibawah ini adalah yang bisa lakukan oleh Admin Sistem Aplikasi Absen Pegawai </p>
 
     </div>
-
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Data Pegawai</h5>
-    <p class="card-text">Admin dapat melakukan penambahan , penghapusan, mengunduh data pegawai pada menu Data Pegawai.</p>
-    <a href="/employee" class="btn btn-primary">Data Pegawai</a>
-  </div>
+    <div class="container">
+            <div class="row">
+                <div class="col">
+                  <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                      <h5 class="card-title">Data Pegawai</h5>
+                        <p class="card-text">Admin dapat melakukan penambahan , penghapusan, mengunduh data pegawai pada menu Data Pegawai.</p>
+                       <a href="/employee" class="btn btn-primary">Data Pegawai</a>
+                     </div>
+                </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                 <h5 class="card-title">Data Absen</h5>
+                    <p class="card-text">Admin dapat melakukan penambahan , penghapusan, mengunduh data absensi pada menu Data Absensi.</p>
+                    <a href="//attendance" class="btn btn-primary">Data Absensi</a>
+                 </div>
+        </div>
+        </div>
+    </div>
+  
 </div>
 
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Data Absen</h5>
-    <p class="card-text">Admin dapat melakukan penambahan , penghapusan, mengunduh data absensi pada menu Data Absensi.</p>
-    <a href="//attendance" class="btn btn-primary">Data Absensi</a>
-  </div>
-</div>
-
-
-</div>
