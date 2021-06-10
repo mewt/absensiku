890025128c2bb863d582be9a086e0491230c2784
mewt <agungsukariman@gmail.com>
2021-06-11 01:23:40 +0700

    update index.php h2

--- views/site/index.php
+++ views/site/index.php
@@ -7,6 +7,11 @@ $this->title = 'Home';
 <div class="site-index">
 
     <div class="jumbotron">
+    
+    <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1">
+        <h2>Hi ,  Admin Selamat Datang di Aplikasi Absensi Pegawai</h2>
+    </div>
+    
     </div>
 
     <div class="body-content">
