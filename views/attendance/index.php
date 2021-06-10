c541edfec6716edc5402145584666abec49ad54a
mewt <agungsukariman@gmail.com>
2021-06-11 02:55:29 +0700

    update attendance index.php content fin

--- views/attendance/index.php
+++ views/attendance/index.php
@@ -91,7 +91,10 @@ $this->params['breadcrumbs'][] = $this->title;
             ExportMenu::FORMAT_TEXT => false,
             ExportMenu::FORMAT_HTML => false,
             ExportMenu::FORMAT_EXCEL => false,
-            ExportMenu::FORMAT_EXCEL_X => false
+            ExportMenu::FORMAT_EXCEL_X => false,
+            ExportMenu::FORMAT_CSV => ['filename' => 'Installation_data'.date('dd-MM-yy')],
+                     ],
+        'filename' => 'Installation_data'.date('dd-MM-yy')
         ],
     ]);
     ?>
