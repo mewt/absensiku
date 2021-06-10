5d5ce29b271c001f28b3621790327800031bba95
mewt <agungsukariman@gmail.com>
2021-06-11 01:29:11 +0700

    update index.php h2

--- views/site/index.php
+++ views/site/index.php
@@ -13,6 +13,13 @@ $this->title = 'Home';
 
     <div class="body-content">
 
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a ultrices ligula, non dapibus ipsum. Vivamus et tincidunt dolor. Vestibulum commodo hendrerit dui, nec pretium enim ornare laoreet. Pellentesque bibendum eget est in feugiat. In sit amet nisl justo. Etiam cursus in dui aliquet rhoncus. Suspendisse vel urna in arcu gravida imperdiet eu a dui. Morbi tincidunt sapien eu dolor accumsan, varius blandit justo eleifend. Quisque in arcu ac nunc fringilla aliquam.
+    </p>
+    <p>
+    Phasellus purus odio, dictum nec finibus in, consectetur et ante. Nullam id diam in arcu convallis mattis id non nulla. Sed a finibus arcu, sed tempus metus. Suspendisse auctor finibus ipsum in laoreet. Integer ut venenatis leo. Mauris tincidunt, risus ac dictum maximus, justo diam iaculis odio, in elementum lorem sem ac elit. Sed tristique volutpat accumsan. Proin condimentum convallis lacinia. Fusce et dui ut nibh faucibus cursus. Donec egestas nibh venenatis, ultricies mauris ut, bibendum turpis. Pellentesque scelerisque semper ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean rhoncus suscipit risus sed fringilla. Cras tempus libero eget pellentesque placerat. Curabitur nec mi mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    </p>
+
         
 
     </div>
