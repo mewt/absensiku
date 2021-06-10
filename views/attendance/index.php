a01f0d1ea9a6fb716eec073f4b77f559938de549
mewt <agungsukariman@gmail.com>
2021-06-11 03:29:07 +0700

    update attendance index.php content fin

--- views/attendance/index.php
+++ views/attendance/index.php
@@ -91,8 +91,6 @@ $this->params['breadcrumbs'][] = $this->title;
             ExportMenu::FORMAT_HTML => false,
             ExportMenu::FORMAT_EXCEL => false,
             ExportMenu::FORMAT_EXCEL_X => false
-            ExportMenu::FORMAT_PDF => 'pdfConfig' => ['orientation' => 'L' ],
-            ],
         ],
     ]);
     ?>
