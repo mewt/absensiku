32533e62f81ecd4194782a5537e06b26389d9a62
mewt <agungsukariman@gmail.com>
2021-06-11 03:46:16 +0700

    update attendance index.php content fin

--- views/attendance/index.php
+++ views/attendance/index.php
@@ -92,7 +92,7 @@ $this->params['breadcrumbs'][] = $this->title;
             ExportMenu::FORMAT_EXCEL => false,
             ExportMenu::FORMAT_EXCEL_X => false,
             ExportMenu::FORMAT_PDF => [
-                'pdfConfig' => ['orientation' => 'L','filename' => 'File_Name -'.date('d-M-Y')] //set mpdf properties here
+                'pdfConfig' => ['orientation' => 'L','filename' => 'Keystone Marketing -'.date('d-M-Y'),] //set mpdf properties here
             
          ]
          
