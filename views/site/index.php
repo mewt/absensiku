aa196780e24b78f0b44e293dc7c5bdd5d40aaa36
mewt <agungsukariman@gmail.com>
2021-06-11 02:14:47 +0700

    update index.php content fin

--- views/site/index.php
+++ views/site/index.php
@@ -9,21 +9,42 @@ $this->title = 'Home';
         <h1>Hi , Admin</h1>
 
         <p class="lead">Selamat Datang di Aplikasi Absensi Pegawai.</p>
-
-    </div>
-    <div class="body-content">
-    <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>
+        <p>
                   Sistem aplikasi absen pegawai adalah sebuah aplikasi yang dapat membantu manajemen dalam melakukan peniliaian performa pegawai berdasarkan presentase kehadiran dan memudahkan pegawai dalam melakukan kegiatan pencatatan kehadiran dan absensi di luar kantor.
                  </p>
                 <p>
                 Sistem aplikasi absen pegawai ini sebagai jawaban terhadap Manajemen Kepegawaian untuk memantapkan administrasi Kepegawaian sebagai upaya untuk memenuhi kebutuhan informasi data pegawai yang cepat, tepat, akuntabel, dan up to date. 
                 </p>
 
+    </div>
+    <div class="body-content">
+    <div class="row">
+            <div class="col-lg-4">
+                <h2>Data Pegawai</h2>
+
+                <p> Admin dapat melakukan edit data, tambah data, hapus data dan cetak laporan pegawai pada menu data pegawai.
+                </p>
                 <p><a class="btn btn-default" href="/employee">Cek Data Pegawai</a></p>
+            </div>
+   
+    </div>
+    <div class="row">
+            <div class="col-lg-4">
+                <h2>Data Absen</h2>
+
+                <p> Admin dapat melakukan edit data, tambah data, hapus data dan cetak laporan pegawai pada menu data absen.
+                </p>
+                <p><a class="btn btn-default" href="/attendance">Cek Data Absens</a></p>
+            </div>
+   
+    </div>
+    <div class="row">
+            <div class="col-lg-4">
+                <h2>Data Absen</h2>
+
+                <p> Admin dapat keluar dari aplikasi dengan menggunakan tombol logout
+                </p>
+                <p><a class="btn btn-default" href="<?php echo Yii::app()->createAbsoluteUrl('site/logout'); ?>">Logout</a></p>
             </div>
    
     </div>
