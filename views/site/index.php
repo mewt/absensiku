898d7f5495b396eaee76ca2af1ac4195373c94c2
mewt <agungsukariman@gmail.com>
2021-06-11 01:39:04 +0700

    update index.php h2

--- views/site/index.php
+++ views/site/index.php
@@ -14,8 +14,18 @@ $this->title = 'Home';
     Sistem aplikasi absen pegawai adalah sebuah aplikasi yang dapat membantu manajemen dalam melakukan peniliaian performa pegawai berdasarkan presentase kehadiran dan memudahkan pegawai dalam melakukan kegiatan pencatatan kehadiran dan absensi di luar kantor.
     </p>
     <p>
-    Sistem Informasi ini sebagai jawaban terhadap Manajemen Kepegawaian untuk memantapkan administrasi Kepegawaian sebagai upaya untuk memenuhi kebutuhan informasi data pegawai yang cepat, tepat, akuntabel, dan up to date. 
+    Sistem aplikasi absen pegawai ini sebagai jawaban terhadap Manajemen Kepegawaian untuk memantapkan administrasi Kepegawaian sebagai upaya untuk memenuhi kebutuhan informasi data pegawai yang cepat, tepat, akuntabel, dan up to date. 
     </p>
+
+    <p>
+    Hal dibawah ini adalah yang bisa lakukan oleh Admin Sistem Aplikasi Absen Pegawai 
+    </p>
+
+    <li>
+    <ul>Admin dapat melakukan penambahan , penghapusan, mengunduh data pegawai pada menu Data Pegawai</ul>
+    <ul>Admin dapat melakukan penambahan , penghapusan, mengunduh data absensi pada menu Data Absensi</ul>
+    <ul>Admin dapat keluar dari sistem dengan aman melalui menu "Logout" </ul>
+    </li>
 
         
 
